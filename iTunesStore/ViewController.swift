72f0f4f0d3972c2fee4393f38bfc61d2b9c7ce73
jiyoungsophia <sophiathedev@gmail.com>
2025-07-29 10:49:40 +0900

    Initial Setting

--- iTunesStore/ViewController.swift
+++ iTunesStore/ViewController.swift
@@ -2,17 +2,17 @@
 //  ViewController.swift
 //  iTunesStore
 //
-//  Created by 김우성 on 7/28/25.
+//  Created by Milou on 7/28/25.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    view.backgroundColor = .brown
+  }
 
 }
-
